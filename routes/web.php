da18fb00c1639f4371d45f2ea2eff8e496f81744
Moiz Haider <ranamoizhaider@icloud.com>
2024-10-11 16:05:53 +0500

    added /videos redirect to new url

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\VideoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [VideoController::class, 'index'])->name('video.index');
+Route::redirect('/videos', '/', 301); // Redirect /videos to the home page
 Route::get('/upload', [VideoController::class, 'showUploadForm'])->name('video.upload.form');
 Route::post('/upload', [VideoController::class, 'uploadVideo'])->name('video.upload');
 Route::get('/play/{videoID}', [VideoController::class, 'playVideo'])->name('video.play');
