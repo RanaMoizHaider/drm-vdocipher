0147335895cedd7c4c168c63f4ca26901c155103
Moiz Haider <ranamoizhaider@icloud.com>
2024-10-11 12:14:23 +0500

    video stream working

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -20,4 +20,33 @@ class VideoController extends Controller
 
         return view('video.index', compact('videos'));
     }
+
+    private function getCredentials($title)
+    {
+        // Calling API to get video credentials
+        $response = Http::asForm()
+            ->withHeaders([
+                'Authorization' => 'Apisecret ' . config('services.vdocipher.key'),
+            ])
+            ->put('https://dev.vdocipher.com/api/videos?title=' . $title);
+
+        return $response->json();
+    }
+
+    public function playVideo($videoID)
+    {
+        $url = 'https://dev.vdocipher.com/api/videos/' . $videoID . '/otp';
+        $response = Http::acceptJson()->contentType('application/json')
+            ->withBody('{
+              "ttl":300
+            }')
+            ->withHeaders([
+                'Authorization' => 'Apisecret ' . config('services.vdocipher.key'),
+            ])
+            ->post($url);
+
+        $cred = $response->json();
+
+        return view('video.play', compact('cred'));
+    }
 }
--- resources/views/video/play.blade.php
+++ resources/views/video/play.blade.php
@@ -2,16 +2,19 @@
 
     <x-slot:title>Play Video</x-slot:title>
 
-    <x-slot:styles>
-    </x-slot:styles>
-
     <div class="flex justify-center items-center">
         <div class="relative w-full md:w-2/3 lg:w-1/2">
             {{-- Video Here--}}
+            <div style="padding-top:52.73%;position:relative;">
+                <iframe
+                    src="https://player.vdocipher.com/v2/?otp={{ $cred['otp'] }}&playbackInfo={{ $cred['playbackInfo'] }}"
+                    style="border:0;max-width:100%;position:absolute;top:0;left:0;height:100%;width:100%;"
+                    allowfullscreen="true"
+                    allow="encrypted-media"
+                >
+                </iframe>
+            </div>
         </div>
     </div>
-
-    <x-slot:scripts>
-    </x-slot:scripts>
 
 </x-layouts.app>
