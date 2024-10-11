81e0e8b1d0f03d5799741ccaf0dbfb044f664be9
Moiz Haider <ranamoizhaider@icloud.com>
2024-10-11 11:22:56 +0500

    added video routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\VideoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/videos', [VideoController::class, 'index'])->name('video.index');
+Route::get('/video/upload', [VideoController::class, 'showUploadForm'])->name('video.upload.form');
+Route::post('/video/upload', [VideoController::class, 'uploadVideo'])->name('video.upload');
+Route::get('/video/play/{token}', [VideoController::class, 'playVideo'])->name('video.play');
