6ecf17e7944f3594e8dd6a6b0e6dc3e77c2613f8
Moiz Haider <ranamoizhaider@icloud.com>
2024-10-11 11:46:48 +0500

    changed token to video id

--- routes/web.php
+++ routes/web.php
@@ -10,4 +10,4 @@ Route::get('/', function () {
 Route::get('/videos', [VideoController::class, 'index'])->name('video.index');
 Route::get('/video/upload', [VideoController::class, 'showUploadForm'])->name('video.upload.form');
 Route::post('/video/upload', [VideoController::class, 'uploadVideo'])->name('video.upload');
-Route::get('/video/play/{token}', [VideoController::class, 'playVideo'])->name('video.play');
+Route::get('/video/play/{videoID}', [VideoController::class, 'playVideo'])->name('video.play');
