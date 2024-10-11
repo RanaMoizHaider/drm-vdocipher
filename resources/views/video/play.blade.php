cd77ee508e8f31a1d5ec58d4f5872354e7e4b1e4
Moiz Haider <ranamoizhaider@icloud.com>
2024-10-11 11:24:12 +0500

    created video.play blade

---
+++ resources/views/video/play.blade.php
@@ -0,0 +1,17 @@
+<x-layouts.app>
+
+    <x-slot:title>Play Video</x-slot:title>
+
+    <x-slot:styles>
+    </x-slot:styles>
+
+    <div class="flex justify-center items-center">
+        <div class="relative w-full md:w-2/3 lg:w-1/2">
+            {{-- Video Here--}}
+        </div>
+    </div>
+
+    <x-slot:scripts>
+    </x-slot:scripts>
+
+</x-layouts.app>
